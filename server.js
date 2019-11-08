b84f376954340319543c44b5a228af7a7b568269
Patrick Lee <cluster000@gmail.com>
2019-11-08 23:04:49 +0800

    Added model, controller and routes for bootcamps

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+const connectDB = require('./config/db');
 
 // Load environment variables
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Logging middleware in development
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,3 +32,11 @@ app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});

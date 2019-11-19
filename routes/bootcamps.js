1ba5faf3acadd36fd67463559b4adfa2d3593813
Patrick Lee <cluster000@gmail.com>
2019-11-19 23:05:16 +0800

    Finished reviews

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -15,12 +15,14 @@ const router = express.Router();
 
 // Include other resource routers
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 const advancedResults = require('../middlewares/advancedResults');
 const { protect, authorize } = require('../middlewares/auth');
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router
   .route('/')

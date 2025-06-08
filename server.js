e5029f3054b48504a3e408259d7206bed145a617
pulsar1812 <cluster000@gmail.com>
2025-06-08 22:33:38 +0800

    Remove xss

--- server.js
+++ server.js
@@ -1,98 +1,94 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const colors = require('colors');
-const fileupload = require('express-fileupload');
-const cookieParser = require('cookie-parser');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
-const cors = require('cors');
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const fileupload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
 
-const connectDB = require('./config/db');
-const errorHandler = require('./middlewares/error');
+const connectDB = require('./config/db')
+const errorHandler = require('./middlewares/error')
 
 // Load environment variables
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: './config/config.env' })
 
 // Connect to database
-connectDB();
+connectDB()
 
 // Route files
-const bootcamps = require('./routes/bootcamps');
-const courses = require('./routes/courses');
-const auth = require('./routes/auth');
-const users = require('./routes/users');
-const reviews = require('./routes/reviews');
+const bootcamps = require('./routes/bootcamps')
+const courses = require('./routes/courses')
+const auth = require('./routes/auth')
+const users = require('./routes/users')
+const reviews = require('./routes/reviews')
 
-const app = express();
+const app = express()
 
 // Body parser
-app.use(express.json());
+app.use(express.json())
 
 // Cookie parser
-app.use(cookieParser());
+app.use(cookieParser())
 
 // Logging middleware in development
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  app.use(morgan('dev'))
 }
 
 // File upload
-app.use(fileupload());
+app.use(fileupload())
 
 // Sanitize data
-app.use(mongoSanitize());
+app.use(mongoSanitize())
 
 // Set security headers
-app.use(helmet());
-
-// Prevent XSS attacks
-app.use(xss());
+app.use(helmet())
 
 // Rate limiting
 app.use(
   rateLimit({
     windowMs: 10 * 60 * 1000,
-    max: 100
+    max: 100,
   })
-);
+)
 
 // Prevent http parameter pollution
-app.use(hpp());
+app.use(hpp())
 
 // Enable CORS
-app.use(cors());
+app.use(cors())
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps);
-app.use('/api/v1/courses', courses);
-app.use('/api/v1/auth', auth);
-app.use('/api/v1/users', users);
-app.use('/api/v1/reviews', reviews);
+app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/courses', courses)
+app.use('/api/v1/auth', auth)
+app.use('/api/v1/users', users)
+app.use('/api/v1/reviews', reviews)
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold
   )
-);
+)
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
+  console.log(`Error: ${err.message}`.red)
 
   // Close server and exit process
-  server.close(() => process.exit(1));
-});
+  server.close(() => process.exit(1))
+})
